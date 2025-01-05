72eee8c5955e2ea02688790ece6c984053217126
allrealmsoflife <domagoj@tuta.com>
2025-01-05 08:45:17 +0100

    oldAttrs to finalAttrs

---
+++ episodes/2/test-v5.nix
@@ -0,0 +1,21 @@
+let 
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (final: prev: {
+        hello = (prev.hello.override {
+          #fetchurl = throw "not fetchurl";
+        }).overrideAttrs (finalAttrs: {
+          version = "2.12";
+          # src = final.fetchurl {
+          #url = finalAttrs.src.url;
+          #hash = "sha256-jzkukv2sv28wsm18tcqnxoczmlxdyh2idh9rlibh2ya=";
+          #};
+
+          nativebuildinputs = (oldAttrs.nativebuildinputs or []) ++ [
+            final.jq
+          ];
+        });
+      })
+    ];
+  };
+in pkgs.hello
