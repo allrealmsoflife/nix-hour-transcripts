8bc32476ca2556bc280da5185725036db14e2ffd
allrealmsoflife <domagoj@tuta.com>
2025-01-06 05:39:36 +0100

    0th string interpolation examples

--- episodes/0/default.nix
+++ episodes/0/default.nix
@@ -3,9 +3,25 @@
     foo = null;
     "foo.bar" = null;
 
-    foo.bar = null;
+    fooz.bar = null;
     # syntactic sugar for:
     # foo = {
     #   bar = null;
     # };
+
+    int = 10 + 2 / 3 - 10;
+
+    floats = 10.0 + 2.0 / 3 - 10.0;
+
+    variables =
+      let someVariable = 10;
+      in someVariable + 1;
+
+    interpolation = 
+      let someName = "Silvan";
+      in "Hello, ${ someName + " Mosberger" }";
+
+    interpolation' = 
+      let someName = "Silvan";
+      in "Hello, ${ someName } Mosberger";
 }
