6313599609982b37ed329ade4914c8432b0d477b
allrealmsoflife <domagoj@tuta.com>
2025-01-13 06:38:34 +0100

    makefile

--- episodes/0/test.nix
+++
@@ -1,8 +0,0 @@
-let 
-    nixpkgs = builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/master.tar.gz";
-    pkgs = import nixpkgs {};
-    someCPackage = pkgs.stdenv.mkDerivation {
-      name = "someCPackage";
-      src = ./src;
-  };
-in someCPackage
