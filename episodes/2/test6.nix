eb18d4cb538f54b2549a0cf5fb5c24a6cc45aacc
allrealmsoflife <domagoj@tuta.com>
2025-01-14 06:43:01 +0100

    2nd trying to figure out why overriding the hash doesn't work

---
+++ episodes/2/test6.nix
@@ -0,0 +1,25 @@
+let 
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (final: prev: {
+        hello = (prev.hello.override {
+          #fetchurl = throw "not fetchurl";
+        }).overrideAttrs (finalAttrs: {
+            version = "2.12";
+            #src = final.fetchurl {
+            #url = finalAttrs.src.url;
+            #hash = "sha256-jzkukv2sv28wsm18tcqnxoczmlxdyh2idh9rlibh2ya=";
+            #};
+
+            src = finalAttrs.src.overrideAttrs (finalAttrs': {
+              sha256 = "0000000000000000000000000000000000000000000000000000";
+            });
+
+          nativebuildinputs = (finalAttrs.nativebuildinputs or []) ++ [
+            final.jq
+          ];
+        });
+      })
+    ];
+  };
+in pkgs.hello
