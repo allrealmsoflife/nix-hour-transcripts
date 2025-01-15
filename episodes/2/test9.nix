691e2e0a1c557285eab3e2ead61eb413361abe43
allrealmsoflife <domagoj@tuta.com>
2025-01-15 06:26:27 +0100

    2nd overriding output hash

---
+++ episodes/2/test9.nix
@@ -0,0 +1,25 @@
+let 
+  pkgs = import <nixpkgs> {
+    overlays = [
+      (final: prev: {
+        hello = (prev.hello.override {
+          #fetchurl = throw "not fetchurl";
+        }).overrideAttrs (finalAttrs: {
+            version = "2.12";
+            #src = final.fetchurl {
+            #url = finalAttrs.src.url;
+            #hash = "sha256-jzkukv2sv28wsm18tcqnxoczmlxdyh2idh9rlibh2ya=";
+            #};
+
+            src = finalAttrs.src.overrideAttrs (finalAttrs': {
+              outputHash = "";
+            });
+
+          nativebuildinputs = (finalAttrs.nativebuildinputs or []) ++ [
+            final.jq
+          ];
+        });
+      })
+    ];
+  };
+in pkgs.hello
